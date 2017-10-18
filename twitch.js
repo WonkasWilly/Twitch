829da9424d592900fb199f8e01350550ba5590f3
Wonkaswilly <pchoi13370@gmail.com>
2017-10-18 15:18:33 -0700

    Added a bit of logic for the search bar

--- twitch.js
+++ twitch.js
@@ -1,9 +1,29 @@
 $(function() {
-    var streamerId =
-    var streamURL = 'https://api.twitch.tv/kraken/streams/' + streamerId + '?client_id=0vq0ks45nlz5gv55gd937j5cb4lqah';
-
-
-    $.getJSON('https://api.twitch.tv/kraken/streams/?client_id=0vq0ks45nlz5gv55gd937j5cb4lqah', function(data) {
-        console.log(data);
+    $("#search-bar").keyup(function(event){
+        if (event.keyCode === 13) {
+            $("#search-button").click();
+    }
     });
+
+
+    $("#search-button").click(function() {
+
+        const searchTerm = document.getElementById("search-bar").value;
+        let streamUrl = 'https://api.twitch.tv/kraken/streams/?client_id=0vq0ks45nlz5gv55gd937j5cb4lqah&game=' + searchTerm;
+        let allStreamUrl = 'https://api.twitch.tv/kraken/streams/?client_id=0vq0ks45nlz5gv55gd937j5cb4lqah';
+
+        if (searchTerm === "") {
+            $.getJSON(allStreamUrl, function(data) {
+                console.log(allStreamUrl);
+            })
+        }
+        else {
+            $.getJSON(streamUrl, function(data) {
+                console.log(data);
+                console.log(streamUrl);
+                
+            });
+        }
+    })
+
 });
