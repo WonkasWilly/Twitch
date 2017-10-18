ae60f9a1bce3819d28aa9766b84bca053819917d
Phillip Choi <pchoi13370@gmail.com>
2017-10-17 23:36:11 -0700

    figured out how to use the api properly

--- twitch.js
+++ twitch.js
@@ -3,7 +3,7 @@ $(function() {
     var streamURL = 'https://api.twitch.tv/kraken/streams/' + streamerId + '?client_id=0vq0ks45nlz5gv55gd937j5cb4lqah';
 
 
-    $.getJSON('https://api.twitch.tv/kraken/streams/xqcow?client_id=0vq0ks45nlz5gv55gd937j5cb4lqah', function(data) {
+    $.getJSON('https://api.twitch.tv/kraken/streams/?client_id=0vq0ks45nlz5gv55gd937j5cb4lqah', function(data) {
         console.log(data);
     });
 });
