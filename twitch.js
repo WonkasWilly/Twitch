11e10a71ebb03630c9eb0253a25eb95dfe2181f3
Phillip Choi <pchoi13370@gmail.com>
2017-10-20 23:21:20 -0700

    created a grid function

--- twitch.js
+++ twitch.js
@@ -1,4 +1,26 @@
 $(function() {
+
+    //This creates the main display/grid that holds all the data from the Twitch API.
+    function createGrid() {
+        var counter = 0;
+        for (let i=0; i < 5; i++) {
+            var createDiv = document.createElement('div');
+            createDiv.setAttribute('class', 'row');
+            createDiv.setAttribute('id', 'row' + i);
+            document.getElementById('streamer-container').appendChild(createDiv);
+            for (let x=0; x < 4; x++) {
+                var createInnerDiv = document.createElement('div');
+                createInnerDiv.setAttribute('class', 'col card');
+                createInnerDiv.setAttribute('id', 'col' + counter.toString());
+                document.getElementById('row' + i).appendChild(createInnerDiv);
+                counter += 1;
+            }
+        }
+    }
+
+    createGrid();
+
+
     $("#search-bar").keyup(function(event){
         if (event.keyCode === 13) {
             $("#search-button").click();
@@ -15,30 +37,13 @@ $(function() {
         if (searchTerm === "") {
             $.getJSON(allStreamUrl, function(data) {
                 console.log(allStreamUrl);
+                alert('Please type in the game you want to stream.');
             })
         }
         //Outputs 20 streams based on the input data
         else {
             $.getJSON(streamUrl, function(data) {
-
-                console.log(streamUrl);
-                var counter = 0;
-                // This creates the multi-grid layout display for the streams
-                // It also adds ids and classes for every single grid
-                for (let i=0; i < 5; i++) {
-                 var createDiv = document.createElement('div');
-                 createDiv.setAttribute('class', 'row');
-                 createDiv.setAttribute('id', 'row' + i);
-                 document.getElementById('streamer-container').appendChild(createDiv);
-                 for (let x=0; x < 4; x++) {
-                     var createInnerDiv = document.createElement('div');
-                     createInnerDiv.setAttribute('class', 'col card');
-                     createInnerDiv.setAttribute('id', 'col' + counter.toString());
-                     document.getElementById('row' + i).appendChild(createInnerDiv);
-                     counter += 1;
-                 }
-                }
-
+                createGrid();
             });
         }
     })
