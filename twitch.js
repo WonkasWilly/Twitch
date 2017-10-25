97b330f85fa6d39b1d875b2f3290c08366286f9a
Wonkaswilly <pchoi13370@gmail.com>
2017-10-25 15:04:46 -0700

    struggling with a url problem right now

--- twitch.js
+++ twitch.js
@@ -1,9 +1,9 @@
 $(function() {
     const allStreamUrl = 'https://api.twitch.tv/kraken/streams/?client_id=0vq0ks45nlz5gv55gd937j5cb4lqah';
+    const test = document.getElementsByClassName('col');
 
 // When the page first loads, 25 random streams get uploaded.
     $.getJSON(allStreamUrl, function(data) {
-        var test = document.getElementsByClassName('col');
         for (let a = 0; a < test.length; a++) {
             let imgUrl = data.streams[a].preview.medium;
             let imgTag = document.createElement('img');
@@ -16,8 +16,6 @@ $(function() {
         };
     });
 
-
-
 // This allows the enter key to do the same function as clicking
     $("#search-bar").keyup(function(event){
         if (event.keyCode === 13) {
@@ -29,14 +27,28 @@ $(function() {
     $("#search-button").click(function() {
         let searchTerm = document.getElementById("search-bar").value;
         let streamUrl = 'https://api.twitch.tv/kraken/streams/?client_id=0vq0ks45nlz5gv55gd937j5cb4lqah&game=' + searchTerm;
-
+// If the input is left blank, you'll get an alert.
         if (searchTerm === '') {
             alert('Don\'t forget to type in the game you want to watch!');
         }
+// This gives you 25 streams of the stream of your choice.
         else {
-            searchTerm = document.getElementById("search-bar").value;
-            console.log(streamUrl);
-        }
+            $.getJSON(streamUrl, function(data) {
+                $('.col').empty();
+                console.log(data);
+                for (let a = 0; a < test.length; a++) {
+                    let imgUrl = data.streams[a].preview.medium;
+                    let imgTag = document.createElement('img');
+                    let streamerUrl = data.streams[a].channel.url;
+                    console.log(streamerUrl);
+                    imgTag.setAttribute('src', imgUrl);
+                    test[a].append(imgTag);
+                    test[a].addEventListener('click', function() {
+                        window.open(streamerUrl);
+                    })
+                };
+            });
+    }
 
     })
 
