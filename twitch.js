96ef6cb71ded672c21e87f4bfdac8376d7c773a1
Phillip Choi <pchoi13370@gmail.com>
2017-10-24 23:24:00 -0700

    fit images perfectly

--- twitch.js
+++ twitch.js
@@ -1,14 +1,9 @@
 $(function() {
     const allStreamUrl = 'https://api.twitch.tv/kraken/streams/?client_id=0vq0ks45nlz5gv55gd937j5cb4lqah';
-<<<<<<< HEAD
-
-=======
-    var allStreamData;
->>>>>>> f8bd6bc85b8e056b8c04b7ed7ff6df108dcf7bc6
     const searchTerm = document.getElementById("search-bar").value;
     var counter = 0;
 
-<<<<<<< HEAD
+
     $.getJSON(allStreamUrl, function(data) {
         var test = document.getElementsByClassName('col');
         for (let a = 0; a < test.length; a++) {
@@ -21,35 +16,6 @@ $(function() {
         };
     });
 
-
-
-=======
-    //This creates the main display/grid that holds all the data from the Twitch API.
-    function createGrid() {
-        for (let i=0; i < 5; i++) {
-            var createDiv = document.createElement('div');
-            createDiv.setAttribute('class', 'row');
-            createDiv.setAttribute('id', 'row' + i);
-            document.getElementById('streamer-container').appendChild(createDiv);
-            for (let x=0; x < 4; x++) {
-                var createInnerDiv = document.createElement('div');
-                createInnerDiv.setAttribute('class', 'col card');
-                document.getElementById('row' + i).appendChild(createInnerDiv);
-                counter += 1;
-            }
-        }
-    }
-
-
-
-    createGrid();
-    $.getJSON(allStreamUrl, function(data) {
-        allStreamData = data;
-        console.log(allStreamData);
-    });
-    console.log(allStreamData);
-
->>>>>>> f8bd6bc85b8e056b8c04b7ed7ff6df108dcf7bc6
 
 
 // This allows the enter key to do the same function as clicking
